fa78ccba738c733b76353a05fc474589faf518bc
zhangsan <1197526628@qq.com>
2019-06-19 10:47:53 +0800

    登录功能完成

--- SSM_dao/src/main/java/com/itheima/dao/TravellerDao.java
+++ SSM_dao/src/main/java/com/itheima/dao/TravellerDao.java
@@ -4,7 +4,6 @@ import com.itheima.domain.Traveller;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
-//
 public interface TravellerDao {
     @Select("select * from traveller where id in (select travellerId from order_traveller where orderId=#{ordersId})")
     public List<Traveller> findByOrdersId(String OrdersId);
